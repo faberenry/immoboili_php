ff9896ca7a71864bcf447fcc1bdb6e74706ca465
LoCaporale <95622214+LoCaporale@users.noreply.github.com>
2021-12-19 16:13:57 +0100

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,22 @@
-<footer class="footer">
-
-</footer>
+<!--       FOOTER       -->
+        <footer class="page-footer font-small purple pt-4 fixed-bottom">
+          <!-- Footer Links -->
+          <div class="container-fluid text-center text-md-left">
+            <!-- Grid row -->
+            <div class="row">
+              <!-- Grid column -->
+              <div class="col-md-6 mt-md-0 mt-3">
+                <!-- Content -->
+                <h5 class="text-uppercase"> <span class="stileOrange" size="7"> IMMOBOILI </span></h5>
+                <p>Un sito nato nel 2021 per aiutare i Locatori e tutti gli studenti universitari a cercare un appartamento in qualasiasi citta' italiana.</p>
+              </div>
+            </div>
+          </div>
+          <!-- Footer Links -->
+      
+          <!-- Copyright -->
+          <div class="footer-copyright text-center py-3">© 2021 No Copyright: (still work in progess)
+          </div>
+          <!-- Copyright -->
+      
+        </footer>
